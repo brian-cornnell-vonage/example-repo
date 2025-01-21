e8a6360c122949d91a8dc974d17b94fa2d49b367
brian-cornnell-vonage <brian.cornnell@vonage.com>
2025-01-21 09:23:01 -0700

    new file sampling cryptography module

--- PVES-10855.py
+++ PVES-10855.py
@@ -2,6 +2,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 private_pem_data = "xxxxx"
 password_str = "REDACTED"
+
 print(type(password_str))
 p_key = serialization.load_pem_private_key(private_pem_data, password=password_str.encode(),backend=default_backend())
 # p_key = serialization.load_pem_private_key(<private_key>, password=password_str.encode(),backend=default_backend())
